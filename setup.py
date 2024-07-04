a0d1a1c5002cf3e2f45c78b99dd7751764bbbf9f
Nishtha Hooda <nishthahooda@Nishthas-Macbook-air.local>
2024-07-04 18:15:04 +0530

    Your commit message here

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open ("README.md", "r", encoding="utf-8") as fh:
+   long_description = fh.read()
+AUTHOR_NAME = 'BAPPY AHMED'
+SRC_REPO = 'src'
+LIST_OF_REQUIREMENTS = ['sreamlit' ]
+setup(
+    name = SRC_REPO,
+    version = '0.0.1',
+    author = 'NISHTHA',
+    author_email = 'nishthahooda10@gmail.com',
+    description = 'A small example package for movies recommendation',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    package = [SRC_REPO],
+    python_requires = '>=3.7',
+    install_requires = LIST_OF_REQUIREMENTS,
+    )
